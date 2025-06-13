e2a0e6933c65394ed802daa0fc2bddaa1d062c78
Clément Cardonne <clement.cardonne@etu.estia.fr>
2025-06-13 17:29:08 +0200

    Tuto 5 main.c

--- src/main.c
+++ src/main.c
@@ -47,9 +47,12 @@ int main(int argc, char **argv) {
     /* second_line()) function is defined in feature.h and implemented in feature.c */
     second_line( configuration.filenames[0] );
   }
-  /*
-   * TO COMPLETE
-   */
+  else if ( strncmp( configuration.command, "print_pixel", 11 ) == 0 ) {
+    /* second_line()) function is defined in feature.h and implemented in feature.c */
+    int x = atoi(configuration.arguments[0]);
+    int y = atoi(configuration.arguments[1]);
+    print_pixel(configuration.filenames[0], x, y);
+  }
   
   return 0;
 }

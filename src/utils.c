3bbb99b56d375abd9eb839d8baecad9cf3e10340
Clément Cardonne <clement.cardonne@etu.estia.fr>
2025-06-13 17:23:39 +0200

    Tuto 5 utils.c

--- src/utils.c
+++ src/utils.c
@@ -2,8 +2,9 @@
 
 #include "utils.h"
 
-/**
- * @brief Here, you have to define functions of the pixel struct : getPixel and setPixel.
- * 
- */
 
+pixelRGB* get_pixel(unsigned char* data, const unsigned int width, const unsigned int height, const unsigned int n, const unsigned int x, const unsigned int y) {
+    unsigned int index = (y * width + x) * n;
+ 
+    return (pixelRGB*) &data[index];
+}

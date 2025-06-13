1f254e1368c95e4b641948a8ef0894eeb3c6440f
Antoine de Pierre de Bernis <antoine.depierredebernis@etu.estia.fr>
2025-06-13 17:26:43 +0200

    recup

--- src/utils.c
+++ src/utils.c
@@ -2,9 +2,4 @@
 
 #include "utils.h"
 
-pixelRGB* get_pixel(unsigned char* data, const unsigned int width, const unsigned int height, const unsigned int n, const unsigned int x, const unsigned int y) {
-    unsigned int index = (y * width + x) * n;
-
-    return (pixelRGB*) &data[index];
-}
 

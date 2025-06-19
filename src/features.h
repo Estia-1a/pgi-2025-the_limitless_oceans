e8cd5517b54723e69330e7fcbbfb0c81d2b193a8
Clément Cardonne <clement.cardonne@etu.estia.fr>
2025-06-19 21:36:33 +0200

    Tuto 21

--- src/features.h
+++ src/features.h
@@ -23,5 +23,6 @@ void color_desaturate(char *source_path);
 void rotate_cw(char *source_path);
 void rotate_acw(char *source_path);
 void mirror_horizontal(char *source_path);
+void mirror_vertical(char *source_path);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -100,6 +100,9 @@ else if (strncmp(configuration.command, "rotate_acw", 10) == 0) {
 else if (strncmp(configuration.command, "mirror_horizontal", 17) == 0) {
     mirror_horizontal(configuration.filenames[0]);
 }
+else if (strncmp(configuration.command, "mirror_vertical", 15) == 0) {
+    mirror_vertical(configuration.filenames[0]);
+}
   /*
    * TO COMPLETE
    */
